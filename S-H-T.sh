82e79d6e00ac6b5920ac24509a5cd75210be643d
S-H-T7669 <saikohackerteam@gmail.com>
2025-07-25 10:01:31 +0600

    Add files via upload

---
+++ S-H-T.sh
@@ -0,0 +1,31 @@
+clear
+figlet -f big TEAM S-H-T| lolcat
+cyan='[0;36m'
+lightgreen='[1;32m'
+red='[1;31m'
+yellow='[1;33m'
+echo -e $lightgreen "[1m                 We Hack To Defend The Digital World... "
+echo " "
+echo -e $yellow "[1m                         -Terminal Banner "
+echo " "
+echo " "
+echo " "
+echo -e "[1m[33m
+Type Your [31mBanner [33mName[32m :
+
+"
+read varbanner
+echo
+echo -e "[1m[33m
+Type Your Cowsay Name[32m :
+
+ "
+read varcowsay
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+echo "toilet -f big ' $varbanner' -F gay | lolcat" > 84nn3r.txt
+echo
+echo "clear" > cl34r.txt
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+ls
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/bash.bashrc
---
+++ requirement.sh
@@ -0,0 +1,20 @@
+clear
+apt update
+apt upgrade
+apt install figlet -y
+figlet FUCK YOU
+apt install toilet -y
+apt install cowsay -y
+apt install nano -y
+apt install ruby -y
+gem install lolcat
+echo
+echo -e "[1m[36m ////////////////////////////////////////////////////////////////////////////// "
+echo -e "[1m[36m //     _______________    __  ___   _____       __  __    ______           // "
+echo -e "[1m[36m //    /_  __/ ____/   |  /  |/  /  / ___/      / / / /   /_  __/          // "
+echo -e "[1m[36m //     / / / __/ / /| | / /|_/ /   \__ \______/ /_/ /_____/ /            // "
+echo -e "[1m[36m //    / / / /___/ ___ |/ /  / /   ___/ /_____/ __  /_____/ /            // "
+echo -e "[1m[36m //   /_/ /_____/_/  |_/_/  /_/   /____/     /_/ /_/     /_/            // "
+echo -e "[1m[36m //  Telegram  : @SHT7669            Now Run bash S-H-T.sh !!!         // "
+echo -e "[1m[36m //  Developer : RM Rony Ali         Tools Type: PAID  Version: 0.2   // "
+echo -e "[1m[36m ////////////////////////////////////////////////////////////////////// "
